a43a7b7eea4713d8670e10a14052c4b2ca3b0570
Aurora <Montacia@gmail.com>
2016-09-14 19:44:54 +1200

    yay errors

--- js/main.ts
+++ js/main.ts
@@ -1,3 +1,16 @@
+
 function bgChange() {
-    $('body').css({"background":"url('https://maps.googleapis.com/maps/api/streetview?size=2000x2000&location="+$('#address').val()+"&fov=90&heading=235&pitch=10&key=AIzaSyA2gsiXBZZs90Ha2DoPsCCMzmzvf8z8ZMM') no-repeat center center fixed","-webkit-background-size":"cover", "-moz-background-size":"cover", "-o-background-size":"cover", "background-size":"cover"})
+    var address:string = $('#address').val();
+    if (address.length < 1){
+        alert("Please type in a location!");
+    } else {
+        $('body').css({"background":"url('https://maps.googleapis.com/maps/api/streetview?size=2000x2000&location="+address+"&fov=120&key=AIzaSyA2gsiXBZZs90Ha2DoPsCCMzmzvf8z8ZMM') no-repeat center center fixed","-webkit-background-size":"cover", "-moz-background-size":"cover", "-o-background-size":"cover", "background-size":"cover"})
+    }
 }
+
+$(function(){
+    $('form').submit(function(jqEvt){
+        jqEvt.preventDefault();
+        bgChange();
+    })
+})
