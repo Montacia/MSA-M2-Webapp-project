0c98aa9d60f2cdc5edd24829c6f997c787ccc357
Aurora <Montacia@gmail.com>
2016-09-14 22:17:47 +1200

    test

--- js/main.ts
+++ js/main.ts
@@ -2,7 +2,7 @@
 function bgChange() {
     var address:string = $('#address').val();
     if (address.length < 1){
-        alert("Please type in a location!");
+        sweetAlert("Please type in a location!");
     } else {
         $('body').css({"background":"url('https://maps.googleapis.com/maps/api/streetview?size=2000x2000&location="+address+"&fov=120&key=AIzaSyA2gsiXBZZs90Ha2DoPsCCMzmzvf8z8ZMM') no-repeat center center fixed","-webkit-background-size":"cover", "-moz-background-size":"cover", "-o-background-size":"cover", "background-size":"cover"})
     }
