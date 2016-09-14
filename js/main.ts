432da8cc8c5400309924da1f1fb64711ad92890e
Aurora <Montacia@gmail.com>
2016-09-14 18:48:23 +1200

    removed alert

--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,3 @@
 function bgChange() {
-    alert($('#address').val());
     $('body').css({"background":"url('https://maps.googleapis.com/maps/api/streetview?size=2000x2000&location="+$('#address').val()+"&fov=90&heading=235&pitch=10&key=AIzaSyA2gsiXBZZs90Ha2DoPsCCMzmzvf8z8ZMM') no-repeat center center fixed","-webkit-background-size":"cover", "-moz-background-size":"cover", "-o-background-size":"cover", "background-size":"cover"})
 }
