817e79108ed9a045a18390f9240af172a1a8d229
Aurora <Montacia@gmail.com>
2016-09-14 18:46:33 +1200

    lookin good

--- js/main.ts
+++ js/main.ts
@@ -1,1 +1,4 @@
-var location: any;
+function bgChange() {
+    alert($('#address').val());
+    $('body').css({"background":"url('https://maps.googleapis.com/maps/api/streetview?size=2000x2000&location="+$('#address').val()+"&fov=90&heading=235&pitch=10&key=AIzaSyA2gsiXBZZs90Ha2DoPsCCMzmzvf8z8ZMM') no-repeat center center fixed","-webkit-background-size":"cover", "-moz-background-size":"cover", "-o-background-size":"cover", "background-size":"cover"})
+}
