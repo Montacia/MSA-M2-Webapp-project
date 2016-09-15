e5416cbbea9eff3b0ebbb0066545ffc154fd4336
Aurora <Montacia@gmail.com>
2016-09-15 15:27:29 +1200

    wrongkey

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function bgChange() {
 }
 $(function () {
     var appInsights = require("applicationinsights");
-    appInsights.setup("b1aefe0d-bbc2-4d0f-bc9a-222561a53548").start();
+    appInsights.setup("6ce9b5d3-cdff-4b95-b786-1b59de0a1490").start();
     $("h1").fitText(1.3, { minFontSize: '1px', maxFontSize: '34%' });
     $('form').submit(function (jqEvt) {
         jqEvt.preventDefault();
