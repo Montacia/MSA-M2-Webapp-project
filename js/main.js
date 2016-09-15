9da56b77bbe14b0e036a9a413df4d6c16c9e03c0
Aurora <Montacia@gmail.com>
2016-09-15 15:35:36 +1200

    fixing buttons

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ function bgChange() {
     }
 }
 $(function () {
-    var appInsights = require("applicationinsights");
-    appInsights.setup("6ce9b5d3-cdff-4b95-b786-1b59de0a1490").start();
     $("h1").fitText(1.3, { minFontSize: '1px', maxFontSize: '34%' });
     $('form').submit(function (jqEvt) {
         jqEvt.preventDefault();
         bgChange();
     });
 });
+var appInsights = require("applicationinsights");
+appInsights.setup("6ce9b5d3-cdff-4b95-b786-1b59de0a1490").start();
